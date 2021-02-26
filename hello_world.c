c67e0fa2df45fd34d70c14572c95c154bc3ed878
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-02-26 10:46:32 -0600

    Add files via upload

--- hello_world.c
+++ hello_world.c
@@ -1,25 +1,10 @@
  #include <stdio.h>
- main()
- {
-     printf("hello, world\n\n");
+ main() {
+    
+	printf("hello, world\n");
 
- float fahr, celsius;
- #define LOWER 0
- #define UPPER 300
- #define STEP 20
- fahr = 0;
-
-    printf ("Farenheit\tCelsius\n");
-
- while (fahr <= UPPER) {
-    celsius = (5/9.0) * (fahr-32.0);
-        printf("%3.0f\t\t %4.1f\n", fahr, celsius);
-        fahr = fahr + STEP;
- }
-    printf("\n\n");
-
- for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
-    printf("%3.0f \t\t %5.1f\n", fahr, (5.0/9.0)*(fahr-32));
- }
-
+ 	printf("hello, ");
+ 	printf("world");
+ 	printf("\n");
+ 	
 }
