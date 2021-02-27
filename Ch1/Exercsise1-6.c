fdf845d409d62d022271d09c17788e9f613a90a1
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-02-27 13:10:26 -0600

    Delete Exercsise1-6.c

--- Ch1/Exercsise1-6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
- main() {
-	
-	printf ("The value of getchar() != EOF is 0 when the file ends, if not, the value is 1\n");
-	while (getchar() != EOF)
-		printf ("Value of getchar() != EOF: %i\n", getchar() != EOF);
-
- } 
