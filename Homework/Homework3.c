4f758e9ab2ca6adb5a261ac2010eaa08b47c97c0
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-03-10 13:56:00 -0600

    Update Homework3.c

--- Homework/Homework3.c
+++ Homework/Homework3.c
@@ -5,7 +5,6 @@
 #define YES 1
 #define NUMBER 2
 #define DONE 5
-#define ADD 4
 #define ONE 12
 
 main () {  
@@ -13,26 +12,36 @@ main () {
 	int c, skip, numb, all, quo;
 	
 	while ((c = getchar()) != EOF) {
-
 		if ((c == '"' || c == '\'') && quo == NO) { //start quotes
-			all = ADD;
+			all = YES;
 			quo = ONE;
-		}	else if ((c == '"' || c == '\'' || c == '\n') && quo == ONE) { //finish quotes
-				all = NO; 
-				quo = NO;
+		} else if ((c == '"' || c == '\'' || c == '\n') && quo == ONE) { //finish quotes
+			all = NO; 
+			quo = NO;
 		}	
-			
-		if (c == '<' || c == '(' || c == '[' || c == '{') //start parenthesis
-			all = ADD;
-		else if (c == '>' || c == ')' || c == ']' || c == '}' || c == '\n') //finish parenthesis
-			all = NO;
+		if (c == '<') //start parenthesis
+			all = 1;
+		else if ((c == '>' || c == '\n') && all == 1) //end parenthesis
+			all = 2;
+		if (c == '(') //start parenthesis
+			all = 3;
+		else if ((c == ')' || c == '\n') && all == 3) //end parenthesis
+			all = 4;
+		if (c == '{') //start parenthesis
+			all = 5;
+		else if ((c == '}' || c == '\n') && all == 5) //end parenthesis
+			all = 6;
+		if (c == '[') //start parenthesis
+			all = 7;
+		else if ((c == ']' || c == '\n') && all == 7) //end parenthesis
+			all = 8;
 	
 		if (c == ',' || c == '.' || c == ':' || c == ';' || c == '?' || c == '!' || c == '_') //remove punctuation
-				skip = NO;
-				else 
-					skip = YES;
+			skip = NO;
+		else 
+			skip = YES;
 			
-		if (all == NO) { //chages the digits to X
+		if ((all % 2) == NO) { //chages the digits to X
 			if (isdigit(c))
 				skip = NUMBER;
 			if (!isdigit(c) && numb == DONE)
@@ -46,9 +55,7 @@ main () {
 			}
 		}
 		
-		if (all == ADD && skip == YES)//prints everything exept puntuation
-			putchar (c);
-			
+		if ((all % 2) != NO && skip == YES)//prints everything exept puntuation
+			putchar (c);		
 	} 
-
 }
