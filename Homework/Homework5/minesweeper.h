c34acc80b6660d9f5b7b819006a1fd994ad425c3
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-04-22 10:14:51 -0500

    Homework5

--- Homework/Homework5/minesweeper.h
+++ Homework/Homework5/minesweeper.h
@@ -1,13 +1,12 @@
 #define forloop(i, x) for(i = 0; i != x; i++)//i used it a lot so just to make it easier
 #define BOMB -1 //set bombs
-#define AMBOMBS 15//amount of bombs
 #define CONTL 10 //number of rows and columns (counter line)
 
 #define LOST 'l' 
 #define WON 'O'
 #define OK '?'
 
-void bombs();
+void bombs(int);
 void neighbours(int); 
 int open();
 void kaboom();
