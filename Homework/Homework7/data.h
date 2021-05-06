1fe2f132f033e83e721b9adc3e0cf1c04f04d7df
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-05-06 14:38:28 -0500

    Homework7

--- Homework/Homework7/data.h
+++ Homework/Homework7/data.h
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <time.h>
+
+#define MAX 25
+
 int calcHorasLaboradas(int pin);
 int crearRegistro(void);
 int checkPines(int pin);
-int verAsistencias(int pin);
-int verTiempoTrabajado(int pin);
-//*En colaboración con Abner*
+
