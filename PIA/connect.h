d4d5c361178b6ef83ac735fa960800a40999d3a4
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-05-15 19:00:00 -0500

    PIA

---
+++ PIA/connect.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int get(int);
+void insert(int, int, int);
+int win();
+int vertical(int, int, int);
+int horizontal(int, int, int);
+int diagonal(int, int, int);
+int diagonal2(int, int, int);
+int random2();
+
+int game[6][7];
+int winner, mode, dif;
+char nom[20], nom2[20];
