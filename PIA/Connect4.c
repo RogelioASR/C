e0552f5d006a134ebce2692a8f9bc110820c13ab
RogelioASR <78610804+RogelioASR@users.noreply.github.com>
2021-05-18 22:06:23 -0500

    PIA

--- PIA/Connect4.c
+++ PIA/Connect4.c
@@ -1,133 +1,111 @@
+/* How to compile:
+Go to onlineGDB or other compiler and add the 3 files:
+connec4.c
+connect.h
+four.c */
+
 #include<stdio.h>
+#include<time.h>
 #include"connect.h"
 
 int main() {
 	
-	int a, b, again = 1, op;
-	char one, two;
+	time_t tiempo = time(0);
+    struct tm *timp = localtime(&tiempo);
+	int mode;
 	
-	do {
+	do {    //MENU
 		printf("Welcome to the Connect four game.\n");
 		printf("-------------------------------------------\n");
-		printf("Choose what option you want:\n1. Read the rules\n");
-		printf("2. Single player mode\n3. Two player mode\n");
+		printf("Choose what option you want:\n1. Single player mode");
+		printf("\n2. Two player mode\n3. Read the rules\n");
 		printf("-------------------------------------------\n");
-		scanf("%i", &op);
+		scanf("%i", &mode);
 
-		switch(op) {
-			case 1: 
+        switch(mode) { //Options of MENU
+        	
+        	case 1: //Select difficulty
+				printf("\nYou have selected single player mode.\nIn what difficulty ");
+				printf("do you want to play?\n1. Hard\n2. Normal\n3. Easy\nOption: "); 
+				scanf("%i", &dif);
+				if(dif < 1 || dif > 3) {
+				    printf("\nInvalid option\n\n");
+					return 0;
+				} break;	
+			
+			case 2: printf("\nYou have selected two player mode.\nHave fun!");
+				break;
+				
+            case 3: 
 				printf("\n\t***RULES***\n");
 				printf("---------------------------------\n");
 				printf("Connect Four is a two-player game in which players alternately place \n");
 				printf("pieces on a vertical board 7 columns across and 6 rows high.\n\n");
 				printf("Each player uses particular pieces, and the object is to be the\n"); 
-				printf("first to obtain four pieces in a horizontal, vertical, or diagonal \n");
-				printf("line.\n\nBecause the board is vertical, pieces inserted in a given column always\ndrop");
-				printf(" to the lowest unoccupied row of that column.\n\n");
-				printf("As soon as a column contains 6 pieces, it is full and no other piece can\nbe placed in the column.");
+				printf("first to obtain four pieces in a horizontal, vertical, or diagonal\nline.");
+				printf("\n\nBecause the board is vertical, pieces inserted in a given column always\n");
+				printf("drop to the lowest unoccupied row of that column.\n\nAs soon as a column ");
+				printf("contains 6 pieces, it is full and no other piece can\nbe placed in the column.");
 				printf("\n\nSo, the game ends when someone conects a 4 line or when the board is full\n");
-				printf("---------------------------------------------------\n\n\n"); break;
-			case 2: mode = 1;
-				printf("\nYou have selected single player mode.\nIn what difficulty do you want to play?");
-				printf("\n1. Hard\n2. Normal\n3. Easy\nOption: "); 
-				scanf("%i", &dif);
-				if(dif != 1 && dif != 2 && dif != 3)
-					printf("\nInvalid option\n\n");
-				else {
-					again = 0;
-					printf("\n\nWhat's your name?\n");
-					scanf("%s", nom);
-					printf("\n\n(Recomended 'X' and 'O')\n");
-					printf("%s, choose the symbol you want to use: ", nom);
-					scanf("%c", &one); scanf("%c", &one);
-					printf("Now, choose the symbol of your oponent: ");
-					scanf("%c", &two); scanf("%c", &two);
-					if(dif == 3) dif = 4;
-				} break;	
-			case 3: again = 0; mode = 2;
-				printf("\nYou have selected two player mode.\nHave fun!");
-				printf("\n\nPlayer one, What's your name?\n");
-				scanf("%s", nom);
-				printf("\nPlayer two, What's your name?\n");
-				scanf("%s", nom2);
-				printf("\n\n(Recomended 'X' and 'O')\n");
-				printf("%s, choose your symbol: ", nom);
-				scanf("%c", &one); scanf("%c", &one);
-				printf("%s, choose your symbol: ", nom2);
-				scanf("%c", &two); scanf("%c", &two);
+				printf("---------------------------------------------------\n\n\n"); 
 				break;
+				
 			default: printf("\nInvalid option\n\n"); return 0;
 		}
-	} while(again);
+	} while(mode == 3);
 	
-	do {
-		printf("\n\n\n");
-		for(a = 0; a < 6; a++) {
-			printf("\t|");
-			for(b = 0; b < 7; b++) {
-				switch(game[a][b]) {
-					case 0: printf("   |"); break;
-					case 1: printf(" %c |", one); break;
-					case 2: printf(" %c |", two); break;
-					case 3: printf("!%c!|", '*'); break;
-				}
-			}
-			printf("\n");
-			if(a == 5) {
-				printf("\t ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯\n");
-				printf("\t  1   2   3   4   5   6   7 \n");
-			}	
-		}
-		if(win())
+	//Players information
+	printf("\n\nPlayer one, What's your name?\n");
+	scanf("%s", pone.name);
+	if(mode == 2) {
+	    printf("\nPlayer two, What's your name?\n");
+	    scanf("%s", ptwo.name); 
+	}
+	printf("\n\nType ONE character and press Enter (Recomended 'X' and 'O')\n");
+	printf("%s, choose your symbol: ", pone.name);
+	scanf("%c", &pone.sym); scanf("%c", &pone.sym);
+	if(mode == 2) 
+	    printf("%s, choose your symbol: ", ptwo.name);
+	else
+	    printf("Now, choose the symbol of your oponent: ");
+	scanf("%c", &ptwo.sym); scanf("%c", &ptwo.sym); 
+	
+	do { //start game
+        
+        board(); //prints board
+        
+		if(win()) //check if win
 			break;
         
-        if(!full()) {
+        if(!full()) { //check if its full
             printf("\nThe board is full, it's a tie\n");
             return 0;
         }
-	} while(get(mode));
+	} while(get(mode)); //gets next move
 	
-	printf("\n\n\n");
-	for(a = 0; a < 6; a++) {
-		printf("\t|");
-		for(b = 0; b < 7; b++) {
-			switch(game[a][b]) {
-				case 0: printf("   |"); break;
-				case 1: printf(" %c |", one); break;
-				case 2: printf(" %c |", two); break;
-				case 3: printf("!%c!|", '*'); break;
-			}
-		}
-		printf("\n");
-		if(a == 5) {
-			printf("\t ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯ ¯¯¯\n");
-			printf("\t  1   2   3   4   5   6   7 \n");
-		}	
-	}
+    board();
 	
-	if(winner == 1 && mode == 1)
-		printf("\nCongratulations %s, You Won!\n", nom);
+	//Winner
+	if(pone.winn == 1)
+		printf("\nCongratulations %s, You Won!\n", pone.name);
 	else if (mode == 1)
-		printf("\nCongratulations %s, You Lost!\n", nom);
-		
-	if(winner == 1 && mode == 2)
-		printf("\nCongratulations %s,  You won!\n", nom);
+		printf("\nCongratulations %s, You Lost!\n", pone.name);
 	else if (mode == 2)
-		printf("\nCongratulations %s, You won!\n", nom2);
+		printf("\nCongratulations %s, You won!\n", ptwo.name);
+	
+	FILE*record = fopen("file.txt", "a+"); //Saves a record in a file 
+    fprintf(record,"----------------------------------------");
+    fprintf(record,"\nLAST GAME PLAYED");
+    fprintf(record,"\n\t\t %02d/%02d/%d", timp->tm_mday-1, timp->tm_mon+1, timp->tm_year+1900);
+    fprintf(record,"\n\t\t    %d:%02d", timp->tm_hour+7, timp->tm_min+1); 
+    fprintf(record,"\n\t%s", (mode == 1) ? "Single player game" : "Two player game");
+    if(mode == 2) {
+        fprintf(record, "\t%s vs %s", pone.name, ptwo.name);
+        fprintf(record,"\n\tWinner: %s", (pone.winn) ? pone.name : ptwo.name);
+    } else
+        fprintf(record,"\n\tWinner: %s", (pone.winn) ? pone.name : "Program");
+    fprintf(record,"\n\t\tThanks for playing\n"); 
+    fclose(record);
+      
 	return 0;
-}
-
-int full() {
-    
-    int a, b, count = 0;
-    
-    for(a = 0; a < 6; a++) {
-		for(b = 0; b < 7; b++) {
-		    if(game[a][b] == 0)
-		        count++;
-		}
-    }
-    
-    return count;
-		    
 }
